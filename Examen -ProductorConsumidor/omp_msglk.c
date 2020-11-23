8e471b8036f3df314b18e74dd08705416a22e1d1
dccopah <39752349+dccopah@users.noreply.github.com>
2020-11-23 18:57:00 -0500

    Add files via upload

---
+++ Examen -ProductorConsumidor/omp_msglk.c
@@ -0,0 +1,101 @@
+// Esta versión utiliza una directiva atómica y bloqueos
+//  OpenMP para proteger las secciones críticas.
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include "queue_lk.h"
+
+const int MAX_MSG = 10000;
+
+void Usage(char* prog_name);
+void Send_msg(struct queue_s* msg_queues[], int my_rank, 
+      int thread_count, int msg_number);
+void Try_receive(struct queue_s* q_p, int my_rank);
+int Done(struct queue_s* q_p, int done_sending, int thread_count);
+
+// Función principal
+int main(int argc, char* argv[]) {
+   int thread_count;
+   int send_max;
+   struct queue_s** msg_queues;
+   int done_sending = 0;
+
+   if (argc != 3) Usage(argv[0]);
+   thread_count = strtol(argv[1], NULL, 10);
+   send_max = strtol(argv[2], NULL, 10);
+   if (thread_count <= 0 || send_max < 0) Usage(argv[0]);
+
+   msg_queues = malloc(thread_count*sizeof(struct queue_node_s*));
+
+#  pragma omp parallel num_threads(thread_count) \
+      default(none) shared(thread_count, send_max, msg_queues, done_sending)
+   {
+      int my_rank = omp_get_thread_num();
+      int msg_number;
+      srandom(my_rank);
+      msg_queues[my_rank] = Allocate_queue();
+
+#     pragma omp barrier //No dejes que ningún hilo envíe mensajes hasta que se construyan todas las colas
+      for (msg_number = 0; msg_number < send_max; msg_number++) {
+         Send_msg(msg_queues, my_rank, thread_count, msg_number);
+         Try_receive(msg_queues[my_rank], my_rank);
+      }
+#     pragma omp atomic
+      done_sending++;
+#     ifdef DEBUG
+      printf("Thread %d > done sending\n", my_rank);
+#     endif
+
+      while (!Done(msg_queues[my_rank], done_sending, thread_count))
+         Try_receive(msg_queues[my_rank], my_rank);
+
+      Free_queue(msg_queues[my_rank]); //Mi cola está vacía y todos terminaron de enviar
+      free(msg_queues[my_rank]);//Por lo tanto, no se volverá a acceder a mi cola y está bien liberarla
+   }  
+
+   free(msg_queues);
+   return 0;
+} 
+
+void Usage(char *prog_name) {
+   fprintf(stderr, "Uso: %s <numero de threads> <numero de mensages>\n",
+         prog_name);
+   fprintf(stderr, "   Numero de messages = numero enviado por cada thread\n");
+   exit(0);
+} 
+
+//Funcion para enviar mensaje
+void Send_msg(struct queue_s* msg_queues[], int my_rank, 
+      int thread_count, int msg_number) {
+// int mesg = random() % MAX_MSG;
+   int mesg = -msg_number;
+   int dest = random() % thread_count;
+   struct queue_s* q_p = msg_queues[dest];
+   omp_set_lock(&q_p->lock);
+   Enqueue(q_p, my_rank, mesg);
+   omp_unset_lock(&q_p->lock);
+#  ifdef DEBUG
+   printf("Thread %d > enviado %d a %d\n", my_rank, mesg, dest);
+#  endif
+} 
+//función para recibir mensaje
+void Try_receive(struct queue_s* q_p, int my_rank) {
+   int src, mesg;
+   int queue_size = q_p->enqueued - q_p->dequeued;
+
+   if (queue_size == 0) return;
+   else if (queue_size == 1) {
+      omp_set_lock(&q_p->lock);
+      Dequeue(q_p, &src, &mesg);  
+      omp_unset_lock(&q_p->lock);
+   } else
+      Dequeue(q_p, &src, &mesg);
+   printf("Thread %d > recibido %d desde %d\n", my_rank, mesg, src);
+} 
+int Done(struct queue_s* q_p, int done_sending, int thread_count) {
+   int queue_size = q_p->enqueued - q_p->dequeued;
+   if (queue_size == 0 && done_sending == thread_count)
+      return 1;
+   else 
+      return 0;
+}  
